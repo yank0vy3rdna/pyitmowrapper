01d3116264a20628ee6173cb0e1d5f994675b9db
yank0vy3rdna <bibiwa2001@gmail.com>
2020-02-20 12:35:37 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyitmowrapper',
+    version='1.0',
+    packages=['tests', 'pyitmowrapper'],
+    url='https://github.com/yank0vy3rdna/pyitmowrapper',
+    license='',
+    author='yank0vy3rdna',
+    author_email='bibiwa2001@gmail.com',
+    description=''
+)
